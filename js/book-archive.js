4a4bafea68eb659f83908f4c118f56d733a17a35
i-rehena14 <rehenaa14@gmail.com>
2021-09-02 20:15:54 +0600

    book result added

--- js/book-archive.js
+++ js/book-archive.js
@@ -1,1 +1,30 @@
-console.log('book');
+// search books
+const searchBooks = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    searchField.value = '';
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayResult(data.docs))
+}
+
+const displayResult = books => {
+    // console.log(books);
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+
+    books.forEach(book => {
+        console.log(book);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <h1>${book.title}</h1> 
+            <p>Author:${book.author_name}</p>
+            <p>First published:${book.publish_date}</p>       
+            <p>Publisher:${book.publisher}</p>       
+        `;
+        searchResult.appendChild(div);
+    });
+}
